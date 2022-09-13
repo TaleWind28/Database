37066866b88ea4809967f12647047ea2c16342f0
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2022-09-13 22:15:24 +0200

    Migliorato il modo per modificare il database
    Aggiunte le funzioni add, update e del

--- codice.js
+++ codice.js
@@ -4,24 +4,17 @@ const db = new sqlite3.Database(`./Database`,sqlite3.OPEN_READWRITE,errore1());
 
 //db.run(`CREATE TABLE personaggi(nome,livello,classe,pv,mana)`); crea il database
 const sql = `INSERT INTO personaggi (nome, livello, classe, pv, mana) VALUES(?,?,?,?,?)`;//crea una riga nel database scelto
-const sqls = `SELECT * FROM personaggi`;//scorre tutto il database e lo logga
+const sqls = `SELECT * FROM personaggi`;//scorre tutto il database
 const upsql = `UPDATE personaggi SET livello = ? WHERE livello = ?`;// aggiorna il valore del SET in base al valore del WHERE  
 const dsql = `DELETE FROM personaggi WHERE nome = ?`//cancella dal database in base al valore del WHERE
 
-function errore(err){if (err) return console.error(err.message);}
-function errore1(err){if (err) return console.error(err.message);console.log("connection successfull")}
-function errore2(err){if (err) return console.error(err.message);console.log("Personaggio Aggiunto")}
-//db.run(sql, ["rosso", 6, "tamer", 58, 72], errore2);
-//db.run(sql, ["viridi", "6", "mago", 54, 72], (err) => {if (err) return console.error(err.message); console.log( "Personaggio Aggiunto")});
-//db.run(sql, ["aranel", "6", "magus", 70, 60], (err) => {if (err) return console.error(err.message); console.log( "Personaggio Aggiunto")});
+function errore(err){if (err) return console.error(err.message);};
+function errore1(err){if (err) return console.error(err.message);console.log("connection successfull")};
+function errore2(err){if (err) return console.error(err.message);console.log("Personaggio Aggiunto")};
 
-
-/*//update
-db.run(upsql, [6,"6"],errore());
-});
-*/
-//delete
-//db.run(dsql,"rosso", errore())
+function add(nome,livello,classe,pv,mana){db.run(sql, [nome, livello, classe, pv, mana], errore2());}
+function update(p1,p2){db.run(upsql, [p1,p2],errore());}
+function del(p){db.run(dsql,p, errore())}
 
 db.all(sqls,[],(err,rows)=>{
     if (err) return console.errorr(err.message);
