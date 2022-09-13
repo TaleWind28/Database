f9ad6a086eae256ff5fcb6f8530718720824a329
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2022-09-13 21:16:26 +0200

    Aggiunta funzionalità Database
    aggiunto sql update
    aggiunto sql delete
    aggiunti 3 personaggi di prova al database

--- codice.js
+++ codice.js
@@ -6,20 +6,33 @@ const db = new sqlite3.Database(`./Database`,sqlite3.OPEN_READWRITE, (err)=>{
     console.log("connection successfull");
 });
 
-//db.run(`CREATE TABLE personaggi(nome,livello,classe,pv,mana)`);
-const sql = `INSERT INTO personaggi (nome, livello, classe, pv, mana) 
-            VALUES(?,?,?,?,?)`;
+//db.run(`CREATE TABLE personaggi(nome,livello,classe,pv,mana)`); crea il database
+const sql = `INSERT INTO personaggi (nome, livello, classe, pv, mana) VALUES(?,?,?,?,?)`;//crea una riga nel database scelto
+const sqls = `SELECT * FROM personaggi`;//scorre tutto il database e lo logga
+const upsql = `UPDATE personaggi SET livello = ? WHERE livello = ?`;// aggiorna il valore del SET in base al valore del WHERE  
+const dsql = `DELETE FROM personaggi WHERE pv = ?`//cancella dal database in base al valore del WHERE
 
-const sqls = `SELECT * FROM personaggi`;
-//db.run(sql, ["red", "6", "tamer", "58", "72"], (err) => {if (err) return console.error(err.message); console.log( "Personaggio Aggiunto")});
-//db.run(sql, ["viridi", "6", "mago", "58", "72"], (err) => {if (err) return console.error(err.message); console.log( "Personaggio Aggiunto")});
-//db.run(sql, ["aranel", "6", "magus", "70", "60"], (err) => {if (err) return console.error(err.message); console.log( "Personaggio Aggiunto")});
+//db.run(sql, ["rosso", 6, "tamer", 58, 72], (err) => {if (err) return console.error(err.message); console.log( "Personaggio Aggiunto")});
+//db.run(sql, ["viridi", "6", "mago", 54, 72], (err) => {if (err) return console.error(err.message); console.log( "Personaggio Aggiunto")});
+//db.run(sql, ["aranel", "6", "magus", 70, 60], (err) => {if (err) return console.error(err.message); console.log( "Personaggio Aggiunto")});
 
+
+/*//update
+db.run(upsql, [6,"6"],function (err){
+    if (err) return console.error(err.message);
+});
+*/
+/*//delete
+db.run(dsql,"70", function (err){
+    if (err) return console.errorr(err)
+})
+*/
 db.all(sqls,[],(err,rows)=>{
     if (err) return console.errorr(err.message);
     rows.forEach(element => {
         console.log(element);
     });
  });
+
 
 db.close((err) => {if (err) return console.error(err.message);});
