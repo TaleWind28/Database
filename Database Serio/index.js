f983fe7756fffdb81813bc6fb12bc6087d0bc821
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2022-09-27 00:03:24 +0200

    Aggiunte varie per il funzionamento del programma
    aggiunta classe skill
    aggiunta classe inventario
    aggiunta classe stat
    aggiunta classe item
    aggiunta classe magia
    aggiunta classe vita
    aggiunto file razza che diventerà un database
    aggiunto file classe che diventerà un database

--- Database Serio/index.js
+++ Database Serio/index.js
@@ -46,34 +46,141 @@ addspell("caduta morbida",1,"tutti",5,"chiedere al master","nessuno");
 */
 
 //mostra tutte le spell
+/*
 spelldb.all(spellsqls,[],(err,rows)=>{
     if (err) return console.errorr(err.message);
     rows.forEach(element => {
         console.log(element);
     });
 });
+*/
+//classe skill che contiene le skill di base dei pg
+class Skill{
+    constructor(acrobatics,appraise,bluff,climb,craft,diplomacy,disable_device,disguise,escape_artist,fly,handle_animal,heal,intimidate,knowledge,linguistics,perception,perform,profession,ride,sense_motive,sleight_of_hand,spellcraft,stealth,survival,swim,use_magic_device){
+        this.acrobatics = acrobatics;
+        this.appraise = appraise;
+        this.bluff = bluff;
+        this.climb = climb;
+        this.craft = craft;
+        this.diplomacy = diplomacy;
+        this.disable_device = disable_device;
+        this.disguise = disguise;
+        this.escape_artist = escape_artist;
+        this.fly = fly;
+        this.handle_animal  =handle_animal;
+        this.heal = heal;
+        this.intimidate = intimidate;
+        this.knowledge = knowledge;
+        this.linguistics = linguistics;
+        this.perception = perception;
+        this.perform = perform;
+        this.profession = profession;
+        this.ride = ride;
+        this.sense_motive = sense_motive;
+        this.sleight_of_hand = sleight_of_hand;
+        this.spellcraft = spellcraft;
+        this.stealth = stealth;
+        this.survival = survival;
+        this.swim = swim;
+        this.use_magic_device = use_magic_device;
+    }
+}
 
+class Statistica{
+    constructor(val){
+        this.val = val;
+        this.mod = (val-10)/2;
+    }
+}
+class Stat{
+    constructor(forza,carisma,costituzione,intelligenza,saggezza,destrezza,bab){
+        this.str = {val:forza.val,mod:forza.mod};
+        this.car = {val:carisma.val,mod:carisma.mod};
+        this.cost = {val:costituzione.val,mod:costituzione.mod};
+        this.int = {val:intelligenza.val,mod:intelligenza.mod};
+        this.wis = {val:saggezza.val,mod:saggezza.mod};
+        this.dex = {val:destrezza.val,mod:destrezza.mod};
+        this.ca = 10+this.dex.mod;
+        this.bab = bab;
+        this.cmb = this.bab+this.str.mod;
+        this.cmd = this.bab+10+this.str.mod+this.dex.mod;
+        this.reflex = this.cost.mod;
+        this.will = this.wis.mod;
+        this.fortitude = this.str.mod;
+    }
+}
+
+//classe inventario che definisce l'inventario del player 
+class Inventario{
+    constructor(oggetti){
+        this.oggetti = oggetti;
+    }
+
+    add(item){
+        this.oggetti[item] = item;
+        return console.log(item.nome,"aggiunto");
+    }
+
+    remove(item){
+        delete this.oggetti[item];
+        return console.log(item.nome,"rimosso");
+    }
+}
+
+//classe item che identifica un oggetto da poter aggiungere nell'inventario
+class Item{
+    constructor(bonus,descrizione){
+        this.bonus = bonus;
+        this.descrizione = descrizione;
+    }
+}
+
+//classe vita che identifica la vita del player
+class Vita{
+    constructor(max){
+        this.max = max;
+        this.current = max;
+    }
+}
+
+//classe Magia che identifica il mana del player
+class Magia{
+    constructor(max){
+        this.max = max;
+        this.current = max;
+    }
+}
+
+rfor = new Statistica(14);
+rcost = new Statistica(16);
+rwis = new Statistica(16);
+rint = new Statistica(16);
+rdex = new Statistica(16);
+rchar = new Statistica(22);
+
+rstat = new Stat(rfor,rchar,rcost,rint,rwis,rdex,3);
 
 //Database Personaggi
-
 const db = new sqlite3.Database(`./Database`,sqlite3.OPEN_READWRITE,errore1());
-//db.run(`CREATE TABLE personaggi(nome,livello,classe,inventario,stat,hp,mana,spell,traits,skill,specials)`);// crea il database
-const sql = `INSERT INTO personaggi (nome,livello,classe,inventario,stat,hp,mana,spell,traits,skill,specials) VALUES(?,?,?,?,?,?,?,?,?,?,?)`;//crea una riga nel database scelto
+//db.run(`CREATE TABLE personaggi(nome,razza,classe,livello,inventario,stat,skill,sig,spell,hp,mana,traits)`);// crea il database
+const sql = `INSERT INTO personaggi (nome,razza,classe,livello,inventario,stat,skill,sig,spell,hp,mana,traits) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)`;//crea una riga nel database scelto
 const sqls = `SELECT * FROM personaggi`;//scorre tutto il database
 const upsql = `UPDATE personaggi SET livello = ? WHERE livello = ?`;// aggiorna il valore del SET in base al valore del WHERE  
-const dsql = `DELETE FROM personaggi WHERE nome = ?`//cancella dal database in base al valore del WHERE
+const dsql = `DELETE FROM personaggi WHERE razza = ?`//cancella dal database in base al valore del WHERE
 
-function add(nome,livello,classe,pv,mana){db.run(sql, [nome, livello, classe, pv, mana], errore2());}
-function update(pc,cond,p1,p2){db.run(upsql, [p1,p2],errore());}
+function add(nome,razza,classe,livello,inventario,stat,skill,sig,spell,hp,mana,traits){db.run(sql, [nome,razza,classe,livello,inventario,stat,skill,sig,spell,hp,mana,traits], errore2());}
+function update(p1,p2){db.run(upsql, [p1,p2],errore());}
 function del(p){db.run(dsql,p, errore())}
-
+//add("red","umano","tamer",6,{},rstat,{},{},{},53,77,"osservato dalla dea dei draghi")
 //mostra tutti i personaggi
 db.all(sqls,[],(err,rows)=>{
     if (err) return console.errorr(err.message);
     rows.forEach(element => {
-        console.log(element);
+        console.log(element);// è possibile usare questo for per vedere le singole proprietà dei personaggi in tabella
     });
 });
 
+console.log(rstat.rfor)
+console.log("stat")
 spelldb.close(errore());
 db.close(errore());
